8bae1fe67da993a3f17056593e1d9b357f09d102
FClavijo00 <fjclavijo00@gmail.com>
2026-09-24 22:22:01 +0200

    header provisional

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,47 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IonTitle, IonHeader, IonToolbar } from '@ionic/angular';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import {
+  IonTitle,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  IonIcon,
+  NavController,
+} from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { arrowBackOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonToolbar, IonHeader],
+  imports: [
+    IonIcon,
+    IonButton,
+    IonBackButton,
+    IonButtons,
+    IonToolbar,
+    IonHeader,
+  ],
 })
 export class HeaderComponent implements OnInit {
   @Input() titulo: string = '';
 
-  constructor() { }
+  private _navCtrl = inject(NavController);
+
+  constructor() {
+    addIcons({
+      arrowBackOutline,
+    });
+  }
+
+  backRecepcion() {
+    this._navCtrl.navigateBack(['recepcion']);
+  }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() { }
+  ngOnInit() {
+    console.log(this.titulo);
+  }
 }
