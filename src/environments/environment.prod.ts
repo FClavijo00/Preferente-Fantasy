cadecb871bb27abfd4c7e48f29ff27c4e9701054
FClavijo00 <fjclavijo00@gmail.com>
2026-09-24 23:46:21 +0200

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseURL: 'https://preferente-fantasy-back.railway.internal/api',
+  apiBaseURL: 'https://preferente-fantasy-back-production.up.railway.app/api',
   storageBucket: 'https://t3.storageapi.dev/integrated-shoebox-gfnvqe/',
 };
