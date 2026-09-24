33d840d43dd5fd116c1213b77ea9fc4b96287305
FClavijo00 <fjclavijo00@gmail.com>
2026-09-24 22:18:34 +0200

    auth Service

--- src/app/core/services/auth-service.service.ts
+++ src/app/core/services/auth-service.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Service } from '@angular/core';
+import { inject, Service, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { NavController } from '@ionic/angular';
 
 interface ApiResponse<T> {
   ok: boolean;
@@ -9,7 +10,7 @@ interface ApiResponse<T> {
 }
 
 interface Usuario {
-  id: string;
+  id: number;
   email: string;
   nombre_usuario: string;
   nombre_club: string;
@@ -21,13 +22,63 @@ interface Usuario {
 @Service()
 export class AuthService {
   private http = inject(HttpClient);
+  private _navCtrl = inject(NavController);
   private apiUrl = `${environment.apiBaseURL}/auth`;
 
+  private readonly STORAGE_KEY = 'pf_user_session';
+
+  public currentUser = signal<Usuario | null>(this.obtenerUsuarioInicial());
+
+  private obtenerUsuarioInicial(): Usuario | null {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    if (!data) return null;
+    try {
+      return JSON.parse(data);
+    } catch {
+      return null;
+    }
+  }
+
+  // Guardar la sesión de forma síncrona en memoria y almacenamiento
+  setSession(user: Usuario, token?: string) {
+    if (token) {
+      localStorage.setItem('pf_token', token);
+    }
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(user));
+    this.currentUser.set(user); // Actualiza la Signal inmediatamente
+  }
+
+  // Cerrar sesión limpiando todo
+  clearSession() {
+    localStorage.removeItem(this.STORAGE_KEY);
+    localStorage.removeItem('pf_token');
+    this.currentUser.set(null);
+  }
+
+  isAuthenticated(): boolean {
+    return this.currentUser() !== null;
+  }
+
   login(email: string, password: string) {
-    return this.http.post<ApiResponse<Usuario>>(`${this.apiUrl}/login`, {
-      email,
-      password,
-    });
+    return this.http.post<ApiResponse<Usuario>>(`${this.apiUrl}/login`, {email, password});
+  }
+
+  getUser(): Usuario | null {
+    return this.currentUser();
+  }
+
+  logout() {
+    this.clearSession();
+    this._navCtrl.navigateRoot(['/login'], { replaceUrl: true });
+  }
+
+  private getUserFromStorage(): Usuario | null {
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  }
+
+  setCurrentUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   register(data: any) {
